5fad77430c36fa15e0f2d6cee67e8f258bbc5afc
Robin de Rooij <rderooij685@gmail.com>
2019-09-27 15:07:00 +0200

    pack.pl: rename github repository
    Apparently, prolog pack has a problem with
    dashes.

--- pack.pl
+++ pack.pl
@@ -2,5 +2,4 @@ name('terminus_store').
 version('0.1').
 keywords(['triple', 'storage']).
 title('Use the Terminus-Store Rust library from Prolog').
-home( 'https://github.com/terminusdb/terminus-store-prolog' ).
-
+home( 'https://github.com/terminusdb/terminus_store_prolog' ).
