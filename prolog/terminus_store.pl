e730edff187150f8e1047686d4369c7e09c19bad
Robin de Rooij <robin@datachemist.com>
2019-09-27 18:19:17 +0200

    Fix unit test for open_directory_store
    The exception was not properly caught

--- prolog/terminus_store.pl
+++ prolog/terminus_store.pl
@@ -6,8 +6,13 @@
 
 :- begin_tests(terminus_store).
 test(open_directory_store_atom) :-
-    open_directory_store(this_is_an_atom),
-    open_directory_store("this is a string").
+    open_directory_store(this_is_an_atom, _),
+    open_directory_store("this is a string", _).
+
+test(open_directory_store_atom_exception) :-
+    catch(open_directory_store(234, _), E, true),
+    print(E),
+    E =@= type_error('We only accept a string or atom as dir_name').
 
 
 :- end_tests(terminus_store).
