739aead14acbbaa0d8c39df08345b7d7b609cebc
Robin de Rooij <robin@datachemist.com>
2019-09-28 10:15:03 +0200

    WIP: memory allocation error on line 45 lib.rs

--- c/terminus_store.h
+++ c/terminus_store.h
@@ -3,4 +3,5 @@ size_t STORE_SIZE;
 size_t DB_SIZE;
 void* open_directory_store(char* dir_name);
 void cleanup_directory_store(void* store);
+void cleanup_db(void* db);
 void* create_database(char* name, void* store, char** err);
