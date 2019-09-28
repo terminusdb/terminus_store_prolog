9a293c36f44f2265c8d4eb60fc34fb9ef1c59d70
Robin de Rooij <robin@datachemist.com>
2019-09-28 12:36:09 +0200

    Add clean function for layer builder

--- c/terminus_store.h
+++ c/terminus_store.h
@@ -4,4 +4,5 @@ size_t DB_SIZE;
 void* open_directory_store(char* dir_name);
 void cleanup_directory_store(void* store);
 void cleanup_db(void* db);
+void cleanup_layer_builder(void* layer_builder);
 void* create_database(char* name, void* store, char** err);
