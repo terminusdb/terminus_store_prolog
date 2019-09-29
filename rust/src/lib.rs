e3341c9909ddedfe007c895c0d4523ff0e7eb944
Matthijs van Otterdijk <matthijs@datachemist.com>
2019-09-29 23:06:00 +0200

    declare the entire rust api as 'unsafe' cause it is

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -14,11 +14,11 @@ use terminus_store::storage::{
 use terminus_store::sync::store::*;
 
 #[no_mangle]
-pub extern "C" fn open_directory_store(
+pub unsafe extern "C" fn open_directory_store(
     dir: *const c_char,
 ) -> *const SyncStore<DirectoryLabelStore, DirectoryLayerStore> {
     // Safe because swipl will always return a null-terminated string
-    let dir_name_cstr = unsafe { CStr::from_ptr(dir) };
+    let dir_name_cstr = CStr::from_ptr(dir);
     let dir_name = dir_name_cstr.to_str().unwrap();
     let store = open_sync_directory_store(dir_name);
     Box::into_raw(Box::new(store))
@@ -29,15 +29,15 @@ fn error_to_cstring(error: io::Error) -> CString {
 }
 
 #[no_mangle]
-pub extern "C" fn create_database(
+pub unsafe extern "C" fn create_database(
     name: *const c_char,
     store_ptr: *mut c_void,
     err: *mut *const c_char,
 ) -> *const SyncDatabase<DirectoryLabelStore, DirectoryLayerStore> {
     let store = store_ptr as *mut SyncStore<DirectoryLabelStore, DirectoryLayerStore>;
-    let store_box = unsafe { Box::from_raw(store) };
+    let store_box = Box::from_raw(store);
     // We assume it to be somewhat safe because swipl will check string types
-    let db_name_cstr = unsafe { CStr::from_ptr(name) };
+    let db_name_cstr = CStr::from_ptr(name);
     let db_name = db_name_cstr.to_str().unwrap();
 
     let result = store_box.create(db_name);
@@ -48,35 +48,26 @@ pub extern "C" fn create_database(
             Box::into_raw(Box::new(database))
         }
         Err(e) => {
-            unsafe {
-                *err = error_to_cstring(e).into_raw();
-            }
+            *err = error_to_cstring(e).into_raw();
             std::ptr::null()
         }
     }
 }
 
 #[no_mangle]
-pub extern "C" fn database_get_head(database_ptr: *mut SyncDatabase<DirectoryLabelStore, DirectoryLayerStore>, err: *mut *const c_char) -> *const SyncDatabaseLayer<DirectoryLayerStore> {
-    let database_box = unsafe { Box::from_raw(database_ptr) };
+pub unsafe extern "C" fn database_get_head(database_ptr: *mut SyncDatabase<DirectoryLabelStore, DirectoryLayerStore>, err: *mut *const c_char) -> *const SyncDatabaseLayer<DirectoryLayerStore> {
+    let database_box = Box::from_raw(database_ptr);
     let result = match database_box.head() {
         Ok(None) => {
-            unsafe {
-                *err = std::ptr::null();
-                std::ptr::null()
-            }
+            *err = std::ptr::null();
+            std::ptr::null()
         },
         Ok(Some(layer)) => {
-            unsafe {
-                *err = std::ptr::null();
-
-                Box::into_raw(Box::new(layer))
-            }
+            *err = std::ptr::null();
+            Box::into_raw(Box::new(layer))
         }
         Err(e) => {
-            unsafe {
-                *err = error_to_cstring(e).into_raw();
-            }
+            *err = error_to_cstring(e).into_raw();
             std::ptr::null()
         }
     };
@@ -123,28 +114,26 @@ pub unsafe extern "C" fn layer_open_write(layer_ptr: *mut SyncDatabaseLayer<Dire
 }
 
 #[no_mangle]
-pub extern "C" fn cleanup_directory_store(store: *mut SyncStore<DirectoryLabelStore, DirectoryLayerStore>) {
-    unsafe { Box::from_raw(store) };
+pub unsafe extern "C" fn cleanup_directory_store(store: *mut SyncStore<DirectoryLabelStore, DirectoryLayerStore>) {
+    Box::from_raw(store);
 }
 
 #[no_mangle]
-pub extern "C" fn cleanup_db(db: *mut SyncDatabase<DirectoryLabelStore, DirectoryLayerStore>) {
-    unsafe { Box::from_raw(db) };
+pub unsafe extern "C" fn cleanup_db(db: *mut SyncDatabase<DirectoryLabelStore, DirectoryLayerStore>) {
+    Box::from_raw(db);
 }
 
 #[no_mangle]
-pub extern "C" fn cleanup_layer(layer: *mut SyncDatabaseLayer<DirectoryLayerStore>) {
-    unsafe { Box::from_raw(layer) };
+pub unsafe extern "C" fn cleanup_layer(layer: *mut SyncDatabaseLayer<DirectoryLayerStore>) {
+    Box::from_raw(layer);
 }
 
 #[no_mangle]
-pub extern "C" fn cleanup_layer_builder(layer_builder: *mut SyncDatabaseLayerBuilder<DirectoryLayerStore>) {
-    unsafe { Box::from_raw(layer_builder) };
+pub unsafe extern "C" fn cleanup_layer_builder(layer_builder: *mut SyncDatabaseLayerBuilder<DirectoryLayerStore>) {
+    Box::from_raw(layer_builder);
 }
 
 #[no_mangle]
-pub extern "C" fn cleanup_cstring(cstring_ptr: *mut c_char) {
-    unsafe {
-        CString::from_raw(cstring_ptr);
-    }
+pub unsafe extern "C" fn cleanup_cstring(cstring_ptr: *mut c_char) {
+    CString::from_raw(cstring_ptr);
 }
