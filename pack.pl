19b98af316c2bdfedeec723449cd3acd5ef671d4
Robin de Rooij <robin@datachemist.com>
2019-10-02 14:44:55 +0200

    pack.pl: change version number

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('terminus_store').
-version('0.3').
+version('0.3.0').
 keywords(['triple', 'storage']).
 title('Use the Terminus-Store Rust library from Prolog').
 home( 'https://github.com/terminusdb/terminus_store_prolog' ).
