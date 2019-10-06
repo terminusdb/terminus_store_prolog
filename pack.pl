f77ef2ffa9439ef884c11226771e2efc009ae2a8
Matthijs van Otterdijk <matthijs@datachemist.com>
2019-10-06 03:21:28 +0200

    version 0.4.1

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('terminus_store').
-version('0.3.0').
+version('0.4.1').
 keywords(['triple', 'storage']).
 title('Use the Terminus-Store Rust library from Prolog').
 home( 'https://github.com/terminusdb/terminus_store_prolog' ).
