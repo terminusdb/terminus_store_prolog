81656b2c41f8977458c9b671ce833ab81188afea
Robin de Rooij <robin@datachemist.com>
2019-10-09 13:42:02 +0200

    0.4.2 release
    rename pack and increment version number

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
-name('terminus_store').
-version('0.4.1').
+name('terminus_store_prolog').
+version('0.4.2').
 keywords(['triple', 'storage']).
 title('Use the Terminus-Store Rust library from Prolog').
 home( 'https://github.com/terminusdb/terminus_store_prolog' ).
