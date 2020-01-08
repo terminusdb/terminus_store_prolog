903b18aa2a6df829cae50cc5d52a5bd4071f9b01
Robin de Rooij <robin@datachemist.com>
2020-01-08 12:13:42 +0100

    Fix deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+docker build -t "terminusdb/terminus_store_prolog:${TRAVIS_TAG}" .
+echo "$DOCKER_PASS" | docker login -u terminusdb --password-stdin
+docker push "terminusdb/terminus_store_prolog:${TRAVIS_TAG}"
+echo "pack_install('https://github.com/terminusdb/terminus_store_prolog/archive/${TRAVIS_TAG}.zip', [interactive=false])." | swipl
