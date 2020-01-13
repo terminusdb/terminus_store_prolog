242bb3cec9e7e260113334d6db571a118547675e
Robin de Rooij <rderooij685@gmail.com>
2020-01-13 10:16:07 +0100

    deploy.sh: see if we can fix pack deployment

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ docker build -t "terminusdb/terminus_store_prolog:${TRAVIS_TAG}" .
 echo "$DOCKER_PASS" | docker login -u terminusdb --password-stdin
 docker push "terminusdb/terminus_store_prolog:${TRAVIS_TAG}"
 echo "pack_install('https://github.com/terminusdb/terminus_store_prolog/archive/${TRAVIS_TAG}.zip', [interactive=false])." | swipl
-docker run -it --rm terminusdb/terminus_store_prolog:latest bash -c "echo 'pack_install(''https://github.com/terminusdb/terminus_store_prolog/archive/${TRAVIS_TAG}.zip'', [interactive=false]).' | swipl"
+docker run -it --rm terminusdb/terminus_store_prolog:latest swipl -g "pack_install('https://github.com/terminusdb/terminus_store_prolog/archive/${TRAVIS_TAG}.zip', [interactive=false])." -g halt
