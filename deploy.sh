f243b3c2397d5fe1854b8bdf93bada3872b17971
Robin de Rooij <robin@datachemist.com>
2020-05-25 13:27:46 +0200

    deploy.sh: don't run tests after deploy since it is redundant

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 docker build -t "terminusdb/terminus_store_prolog:${TRAVIS_TAG}" .
 echo "$DOCKER_PASS" | docker login -u terminusdb --password-stdin
 docker push "terminusdb/terminus_store_prolog:${TRAVIS_TAG}"
-docker run -it --rm terminusdb/terminus_store_prolog:latest swipl -g "pack_remove(terminus_store_prolog), pack_install('https://github.com/terminusdb/terminus_store_prolog/archive/${TRAVIS_TAG}.zip', [interactive=false])." -g halt
