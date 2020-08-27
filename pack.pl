c32b648a6c31dff9ffa5f83168e2356657e85ab1
Robin de Rooij <robin@datachemist.com>
2020-08-27 18:02:00 +0200

    pack.pl: increase version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('terminus_store_prolog').
-version('0.11.2').
+version('0.11.3').
 keywords(['triple', 'storage']).
 title('Use the Terminus-Store Rust library from Prolog').
 home( 'https://github.com/terminusdb/terminus_store_prolog' ).
