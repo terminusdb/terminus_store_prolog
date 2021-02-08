3030c157c6e4a7ea706dc86ba6c8a0d67b940c40
Gavin Mendel-Gleason <gavin@terminusdb.com>
2021-02-08 15:43:54 +0100

    Add pack version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('terminus_store_prolog').
-version('0.15.0').
+version('0.15.1').
 keywords(['triple', 'storage']).
 title('Use the Terminus-Store Rust library from Prolog').
 home( 'https://github.com/terminusdb/terminus_store_prolog' ).
