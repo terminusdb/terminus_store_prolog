0f872ebe5f52e2ac0c66cc748a42dafd6903881a
Matthijs van Otterdijk <matthijs@terminusdb.com>
2021-05-11 17:18:03 +0200

    also check in pack.pl

---
+++ pack.pl
@@ -0,0 +1,5 @@
+name('terminus_store_prolog_rewrite').
+version('0.19.0').
+keywords(['triple', 'storage']).
+title('Use the Terminus-Store Rust library from Prolog').
+home( 'https://github.com/terminusdb/terminus_store_prolog_rewrite' ).
