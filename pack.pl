2b7f3dab0049756f1497c0fbc36b29d64b1e2b5a
Matthijs van Otterdijk <matthijs@terminusdb.com>
2022-05-05 16:58:54 +0200

    Raise pack version to 0.19.8

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('terminus_store_prolog').
-version('0.19.7').
+version('0.19.8').
 keywords(['triple', 'storage']).
 title('Use the Terminus-Store Rust library from Prolog').
 home( 'https://github.com/terminusdb/terminus_store_prolog' ).
